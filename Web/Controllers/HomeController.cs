abcac3e003e0db531fca8c0bb88206a2478c01a3
Marcio Abriola <marcioabriola@gmail.com>
2023-04-14 20:09:21 -0300

    Subo Cambios

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-using Web.Models;
 
 namespace Web.Controllers
 {
@@ -16,17 +15,6 @@ namespace Web.Controllers
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
